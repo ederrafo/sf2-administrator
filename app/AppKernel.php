3be593f876e69f86f6f615b4477b15ecb4465262
eder rafo <ederrafo@gmail.com>
2018-10-19 20:27:36 +0000

    installed sonata-project/admin-bundle and sonata-project/doctrine-orm-admin-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -9,13 +9,30 @@ class AppKernel extends Kernel
     {
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+
+            // The admin requires some twig functions defined in the security
+            // bundle, like is_granted. Register this bundle if it wasn't the case
+            // already.
+            // El administrador requiere algunas funciones de twig definidas en el bundle de seguridad como is_granted. Registra este bundle si no fuera el caso.
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+
+            // These are the other bundles the SonataAdminBundle relies on
+            // Estos son los bundles en que se basa SonataAdminBundle
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+
+            // And finally, the storage and SonataAdminBundle
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
